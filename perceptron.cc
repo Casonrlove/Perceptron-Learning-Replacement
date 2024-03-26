8235ef7d7e657d554976b359c93ed66e38a3d20a
Casonrlove <Casonrlove@tamu.edu>
2024-03-26 15:01:49 -0500

    cleaned code up

--- perceptron.cc
+++ perceptron.cc
@@ -5,32 +5,50 @@
 
 #include "cache.h"
 
+/*************************************************************************/
+/******************************** CLASS **********************************/
+
 class perceptron
 {
+    /* ATTRIBUTES */
+    //perceptron history
+    //percpetron bits
+    //number of perceptrons
 
     public:
 
-        void update()
+        void update(/* HISTORY */)
         {
             
         }
-};
 
+        void predict(/* NEXT REPLACMENT */)
+        {
+
+        }
+};
+/*************************************************************************/
+/******************************* END CLASS *******************************/
+
+
+
+
+
+/*************************************************************************/
+/************************ INITIALIZE REPLACEMENT *************************/
 
 void CACHE::initialize_replacement() { ::last_used_cycles[this] = std::vector<uint64_t>(NUM_SET * NUM_WAY); }
 
-uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
-{
-  auto begin = std::next(std::begin(::last_used_cycles[this]), set * NUM_WAY);
-  auto end = std::next(begin, NUM_WAY);
+/*************************************************************************/
+/**************************** END REPLACEMENT ****************************/
 
-  // Find the way whose last use cycle is most distant
-  auto victim = std::min_element(begin, end);
-  assert(begin <= victim);
-  assert(victim < end);
-  return static_cast<uint32_t>(std::distance(begin, victim)); // cast protected by prior asserts
-}
 
+
+
+
+
+/*************************************************************************/
+/************************** UPDATE REPLACMENT ****************************/
 void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type,
                                      uint8_t hit)
 {
@@ -38,5 +56,15 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
   if (!hit || access_type{type} != access_type::WRITE) // Skip this for writeback hits
     ::last_used_cycles[this].at(set * NUM_WAY + way) = current_cycle;
 }
+/*************************************************************************/
+/*************************** END REPLACMENT ******************************/
 
+
+
+
+
+/*************************************************************************/
+/***************************** FINAL STATS *******************************/
 void CACHE::replacement_final_stats() {}
+/*************************************************************************/
+/*************************** END FINAL STATS *****************************/

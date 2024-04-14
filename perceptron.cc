bd9f197f9a282f6db175fe91f19af6d10c238efc
Cason Love <108733047+Casonrlove@users.noreply.github.com>
2024-04-14 16:39:51 -0500

    added PredictorTable Struct, commiting for backup

--- perceptron.cc
+++ perceptron.cc
@@ -38,7 +38,7 @@ namespace{
     size_t replace_threshold = 124;
     size_t sampler_threshold = 68;
 
-    //sampler entry struct - 
+    //sampler entry struct -
     struct SamplerEntry {
         uint64_t address; //address of our stored cache block. Only use 15 bits to check tags (see sampler for details)
         uint64_t lru_bits; //sampler LRU. For simulation purposes we use current clock cycle, in actuality number of bits used would be NUM_WAYS
@@ -55,14 +55,20 @@ namespace{
     size_t sampler_table_size = sampler_sets * sampler_num_ways;
 
     //TODO: prediction table struct
-
+    struct PredictorTable
+    {
+        /* data */
+    };
+    
 
     //vectors representing physical tables (vectors are mapped to their corresponding cache via its address)
 
     std::map<CACHE*, std::vector<SamplerEntry>> sampler_table;   //sampler vector table - indexed via (set_id * sampler_num_ways + way_id)
     std::map<CACHE*, std::vector<uint64_t>> sampler_cache_sets; //value V is cache set id, index I is sampler set ID.
-    //TODO: prediction table vector
 
+    //TODO: prediction table vector
+    std::map<CACHE*, std::vector<PredictorTable>> predictor_table;
+    
 
     //vectors representing extra cache block information (block 10 would have lru bits stored in entry 10 of this vector)
     std::map<CACHE*, std::vector<uint64_t>> lru_bits; //Predictor lru vector
@@ -81,7 +87,7 @@ namespace{
 /*************************************************************************/
 /************************ INITIALIZE REPLACEMENT *************************/
 
-void CACHE::initialize_replacement() { 
+void CACHE::initialize_replacement() {
     //initialize sampler vector
     ::sampler_table[this] = std::vector<SamplerEntry>(::sampler_sets * ::sampler_num_ways);
 
@@ -120,8 +126,8 @@ uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t
 
 
   //NOTE: code below is from SHIP, similar to what we need to do except instead of using maxRRPV we use above criteria
-  // 
-  // 
+  //
+  //
   // look for the maxRRPV line
   //auto begin = std::next(std::begin(::rrpv_values[this]), set * NUM_WAY);
   //auto end = std::next(begin, NUM_WAY);
@@ -177,7 +183,7 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
 
 
 
-    //TODO: do sampler stuff    
+    //TODO: do sampler stuff
     /* SAMPLER STUFF */
     //check if sampled set, and if we find one then do training
     auto sample = std::find(std::begin(::sampler_cache_sets[this]), std::end(::sampler_cache_sets[this]), set);
@@ -189,7 +195,7 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
 
         //check for tag match in the set
         //very fun lambda function that compares 15 tag bits of sampler address with 15 tag bits of incoming block address.
-        auto compare_addr = [cache_address = full_addr, mask = 15, shift = (OFFSET_BITS + champsim::lg2(::sampler_sets))](auto sampler) 
+        auto compare_addr = [cache_address = full_addr, mask = 15, shift = (OFFSET_BITS + champsim::lg2(::sampler_sets))](auto sampler)
             {return ((cache_address >> shift) & champsim::bitmask(15)) == ((sampler.address >> shift) & champsim::bitmask(15)); };
         auto tag_found = std::find_if(start, end, compare_addr);
 
@@ -214,18 +220,17 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
             }
                 
             //replace sample struct with new struct that has current hashed feature values as well as the current cache block's Yout, tag, and lru (clock cycle)
-            *throwaway_sample = { full_addr, current_cycle, yout, pc_0_hash, pc_1_hash, pc_2_hash, pc_3_hash, tag_1_hash, tag_2_hash };          
+            *throwaway_sample = { full_addr, current_cycle, yout, pc_0_hash, pc_1_hash, pc_2_hash, pc_3_hash, tag_1_hash, tag_2_hash };
         }
     }
 
     //NOTE: for saturating counters, dont forget to make sure they saturate at the algorithm's specifcation
     //  for example, a "6 bit saturating counter" that uses int_32t, should saturate between -32 or 32, not 2 billion or whatever
-  
 
 
   //LRU stuff (backup)
   // Mark the way as being used on the current cycle
-  // 
+  //
   //if (!hit || access_type{type} != access_type::WRITE) // Skip this for writeback hits
    // ::last_used_cycles[this].at(set * NUM_WAY + way) = current_cycle;
 }

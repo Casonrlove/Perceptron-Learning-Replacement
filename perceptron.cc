f303fd9876ff49553bbbd86e3cb25ac76bf122b9
Marshall <marshallmc@tamu.edu>
2024-04-27 15:02:24 -0500

    my testing stuff

--- perceptron.cc
+++ perceptron.cc
@@ -8,19 +8,24 @@
 /*************************************************************************/
 /******************************** CLASS **********************************/
 
-namespace{
+namespace {
+    //debug stuff
+    size_t lru_counter;
+    size_t reuse_counter;
+    size_t sample_count;
+
     /* ATTRIBUTES */
 
     //Vars for keeping track of previous PCs
-    size_t pc_0 = 0;
-    size_t pc_1 = 0;
-    size_t pc_2 = 0;
-    size_t pc_3 = 0;
+    volatile size_t pc_0 = 0;
+    volatile size_t pc_1 = 0;
+    volatile size_t pc_2 = 0;
+    volatile size_t pc_3 = 0;
 
     //thresholds
     constexpr int32_t bypass_threshold = 3;
-    constexpr int32_t replace_threshold = 124;
-    constexpr int32_t sampler_threshold = 68;
+    constexpr int32_t replace_threshold = 127;
+    constexpr int32_t sampler_threshold = 64;
 
     //sampler entry struct -
     struct SamplerEntry {
@@ -46,7 +51,7 @@ namespace{
     std::vector<int32_t> pc_3_feature(256);
     std::vector<int32_t> tag_1_feature(256); // 3)The tag of the current block shifted right by 4,
     std::vector<int32_t> tag_2_feature(256); // 4)The tag of the current block shifted right by 7.
-    
+
     //vectors representing physical tables (vectors are mapped to their corresponding cache via its address)
 
     std::map<CACHE*, std::vector<SamplerEntry>> sampler_table; //sampler vector table - indexed via (set_id * sampler_num_ways + way_id)
@@ -94,10 +99,10 @@ uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t
     //TODO: FIND REPLACEMENT
     //first: check reuse bits for a 0.
     //second (as backup if all are marked for reuse): check backup LRU bits
-    uint64_t pc_0_hash  = ((pc_0 >> 2)      ^ pc_0) & champsim::bitmask(8);
-    uint64_t pc_1_hash  = ((pc_1 >> 1)      ^ pc_0) & champsim::bitmask(8);
-    uint64_t pc_2_hash  = ((pc_2 >> 2)      ^ pc_0) & champsim::bitmask(8);
-    uint64_t pc_3_hash  = ((pc_3 >> 3)      ^ pc_0) & champsim::bitmask(8);
+    uint64_t pc_0_hash = ((pc_0 >> 2) ^ pc_0) & champsim::bitmask(8);
+    uint64_t pc_1_hash = ((pc_1 >> 1) ^ pc_0) & champsim::bitmask(8);
+    uint64_t pc_2_hash = ((pc_2 >> 2) ^ pc_0) & champsim::bitmask(8);
+    uint64_t pc_3_hash = ((pc_3 >> 3) ^ pc_0) & champsim::bitmask(8);
     uint64_t tag_1_hash = ((full_addr >> 4) ^ pc_0) & champsim::bitmask(8);
     uint64_t tag_2_hash = ((full_addr >> 7) ^ pc_0) & champsim::bitmask(8);
 
@@ -119,21 +124,27 @@ uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t
     //else
     //{
         //check for bit not marked for reuse
-        for (uint32_t way = 0; way < NUM_WAY; way++)
+    for (uint32_t way = 0; way < NUM_WAY; way++)
+    {
+        if (::reuse_bits[this].at(set * NUM_WAY + way) == false)
         {
-            if (::reuse_bits[this].at(set * NUM_WAY + way) == false)
-            {
-                return static_cast<uint32_t>(way);
-            }
+            reuse_counter++;
+  //          if (reuse_counter % 10000 == 0)
+  //              printf("\n REUSE: %d", reuse_counter);
+            return static_cast<uint32_t>(way);
         }
+    }
 
-        //as a backup we check LRU
-        auto lru_begin = std::next(std::begin(::lru_bits[this]), set * NUM_WAY);
-        auto lru_end = std::next(lru_begin, NUM_WAY);
-        auto victim = std::min_element(lru_begin, lru_end);  
-        return static_cast<uint32_t>(std::distance(lru_begin,victim));
+    //as a backup we check LRU
+    lru_counter++;
+ //   if (lru_counter % 10000 == 0)
+ //       printf("\n lru: %d", lru_counter);
+    auto lru_begin = std::next(std::begin(::lru_bits[this]), set * NUM_WAY);
+    auto lru_end = std::next(lru_begin, NUM_WAY);
+    auto victim = std::min_element(lru_begin, lru_end);
+    return static_cast<uint32_t>(std::distance(lru_begin, victim));
     //}
-    
+
 }
 
 /*************************************************************************/
@@ -149,17 +160,28 @@ uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t
 void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit)
 {
 
-    if (hit && access_type{type} == access_type::WRITE) { // Skip this for writeback hits
+
+
+
+
+    if (!hit || access_type{ type } != access_type::WRITE) // Skip this for writeback hits
+        ::lru_bits[this].at(set * NUM_WAY + way) = current_cycle;
+
+    if (access_type{ type } == access_type::WRITE) {
         return;
+
     }
-    
-    ::lru_bits[this].at(set * NUM_WAY + way) = current_cycle;
     //NOTE: using bitmask to get rid of extra bits because our theoretical cache only stores 8 bits per feature
     //feature values
     pc_3 = pc_2;
     pc_2 = pc_1;
     pc_1 = pc_0;
     pc_0 = ip;
+
+    sample_count++;
+    // printf("pc0: %lu, pc1: %lu, pc2: %lu, pc3: %lu, ip: %lu\n", pc_0, pc_1, pc_2, pc_3, ip);
+
+    ::lru_bits[this].at(set * NUM_WAY + way) = current_cycle;
 
     //feature hashes
     uint64_t pc_0_hash = ((pc_0 >> 2) ^ pc_0) & champsim::bitmask(8);
@@ -168,6 +190,7 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
     uint64_t pc_3_hash = ((pc_3 >> 3) ^ pc_0) & champsim::bitmask(8);
     uint64_t tag_1_hash = ((full_addr >> 4) ^ pc_0) & champsim::bitmask(8);
     uint64_t tag_2_hash = ((full_addr >> 7) ^ pc_0) & champsim::bitmask(8);
+
 
     //use hashed features as index in feature table to access weights and sum all the weights and calc yout
     //since weights are signed, summation is signed
@@ -203,12 +226,12 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
         auto compare_addr = [cache_address = full_addr, mask = 15, shift = (OFFSET_BITS + champsim::lg2(::sampler_sets))](auto sampler)
             {return ((cache_address >> shift) & champsim::bitmask(15)) == ((sampler.address >> shift) & champsim::bitmask(15)); };
         auto tag_found = std::find_if(start, end, compare_addr);
-
         //if tag in sampler, we update yout, update input hash values, update LRU value, and then update weights based on prediction
         if (tag_found != end) {
             //decrement weights of feature table for current features if Yout is above the theta threshold
             SamplerEntry* old_sample = &(*tag_found);
-            if (old_sample->yout > sampler_threshold) {
+
+            if (old_sample->yout > -sampler_threshold) {
                 if (::pc_0_feature[pc_0_hash] > -32) {
                     ::pc_0_feature[pc_0_hash]--;
                 }
@@ -257,7 +280,7 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
                     ::tag_2_feature[throwaway_sample->tag_shift_2]++;
                 }
             }
-                
+
             //replace sample struct with new struct that has current hashed feature values as well as the current cache block's Yout, tag, and lru (clock cycle)
             *throwaway_sample = { full_addr, current_cycle, yout, pc_0_hash, pc_1_hash, pc_2_hash, pc_3_hash, tag_1_hash, tag_2_hash };
         }
